8d526e7411eb5af8a31d7f947961b9265fe60523
dibsypeter7 <dipaputra28@gmmail.com>
2025-09-24 11:14:26 +0700

    dipa suki

---
+++ punyadipa.java
@@ -0,0 +1,8 @@
+public class punyadipa {
+    public static void main(String[] args) {
+        
+        System.out.println("Halo namaku dipa asal saya blitar nomer hp ku 081334185645");
+        
+    }
+    
+}
